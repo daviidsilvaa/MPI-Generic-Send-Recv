6f2f2a15f3e8569357b1179cc1cbe90e4a1df809
daviidsilvaa <davidsfernandes2@gmail.com>
2017-11-24 18:30:15 -0200

    Update Fork and Join

--- Model.hpp
+++ Model.hpp
@@ -4,6 +4,8 @@
 
 #include "CellularSpace.hpp"
 #include "Flow.hpp"
+#include "MPIImpl.hpp"
+#include "assert.h"
 #include <iostream>
 using namespace std;
 
@@ -52,6 +54,7 @@ public:
         MPI_Comm_size(mpi_comm, &comm_size);
         MPI_Comm_rank(mpi_comm, &comm_rank);
         MPI_Status mpi_status;
+        MPI_Request mpi_send_request, mpi_recv_request;
         comm_workers = comm_size - 1;
 
         if(comm_rank == 0){
@@ -82,7 +85,17 @@ public:
                 MPI_Send(word_execute_send, 23, MPI_CHAR, dest, 999, mpi_comm);
             }
 
+            R temp, acumulated_value_recv = 0;
+            for(int source = 1; source < comm_size; source++){
+                MPI_Recv(&temp, 1, ConvertType(getAbstractionDataType<R>()), source, source, MPI_COMM_WORLD, &mpi_status);
+                acumulated_value_recv += temp;
+            }
+
+            // Verifica se os valores simulados foram conservados
+            assert((acumulated_value_recv - 10000) < 0.001);
         }
+
+        // Executa a simulacao nas maquinas slayers
         if(comm_rank != 0){
             char word_cs_recv[23], word_execute_recv[23];
 
@@ -117,15 +130,80 @@ public:
             int y_ = atoi(y_c);
             int flow_rate_ = atoi(flow_rate_c);
 
+            // corrigir x_init
+            // corrigir endereçamento de cell
+            // corrigir acesso a cell
+            // subtrair flow
+            // incrementar flow
+
             if(comm_rank == rank_){
-                cout << cs.memoria[(x_%cs.width)*cs.height + y_].x << " " << cs.memoria[(x_%cs.width)*cs.height + y_].y
-                    << " " << cs.memoria[(x_%cs.width)*cs.height + y_].count_neighbors << endl;
+                cout << cs.memoria[x_*cs.width + y_ - cs.x_init].x << " " << cs.memoria[x_*cs.width + y_ - cs.x_init].y
+                    << " " << cs.memoria[x_*cs.width + y_].count_neighbors << endl;
+
+                //for(double t = 0; t < this->time; t = t + this->time_step){
+                    this->flow.last_execute = this->flow.execute();
+                    cout << comm_rank << ": " << this->flow.execute() << endl;
+                // }
+
+                int count_neighbors_send, y_send;
+                R last_execute_send;
+
+                // Atualizando o valor do atributo na maquina vizinha
+                if(cs.memoria[x_*cs.width + y_ - cs.x_init].x - cs.x_init == PROC_DIMX-1){
+                    switch(cs.memoria[x_*cs.width + y_ - cs.x_init].count_neighbors){
+                        case 3:
+                            cout << __FILE__ << ": " << __LINE__ << endl;
+                            break;
+                        case 5:
+                            cout << __FILE__ << ": " << __LINE__ << endl;
+                            break;
+                        case 8:
+                            count_neighbors_send = 3;
+                            last_execute_send = this->flow.last_execute/cs.memoria[x_*cs.width + y_ - cs.x_init].count_neighbors;
+                            y_send = cs.memoria[x_*cs.width + y_ - cs.x_init].y - 1;
+
+                            MPI_Send(&count_neighbors_send, 1, MPI_INT, rank_+1, rank_, MPI_COMM_WORLD);
+                            MPI_Send(&last_execute_send, 1, ConvertType(getAbstractionDataType<R>()), rank_+1, rank_, MPI_COMM_WORLD);
+                            MPI_Send(&y_send, 1, MPI_INT, rank_+1, rank_+10, MPI_COMM_WORLD);
+
+                            cs.memoria[x_*cs.width + y_-1 - cs.x_init].attribute.value += last_execute_send;
+                            cs.memoria[x_*cs.width + y_+1 - cs.x_init].attribute.value += last_execute_send;
+                            for(int i = 0; i < count_neighbors_send; i++)
+                                cs.memoria[(x_-1)*cs.width + y_-1+i - cs.x_init].attribute.value += last_execute_send;
+
+                            cs.memoria[x_*cs.width + y_ - cs.x_init].attribute.value -= this->flow.last_execute;
+
+                            break;
+                        default:
+                            cout << __FILE__ << ": " << __LINE__ << endl;
+                    }
+                }
+
+                if(cs.memoria[x_*cs.width + y_ - cs.x_init].x - cs.x_init == PROC_DIMX){
+                    cout << __FILE__ << ": " << __LINE__ << endl;
+                }
             }
 
-            for(double t = 0; t < this->time; t = t + this->time_step){
-                // cout << comm_rank << ": " << this->flow.execute() << endl;
-                this->flow.last_execute = this->flow.execute();
+            if(comm_rank == rank_+1){
+                int count_neighbors_recv, y_recv;
+                R last_execute_recv;
+
+                MPI_Recv(&count_neighbors_recv, 1, MPI_INT, rank_, rank_, MPI_COMM_WORLD, &mpi_status);
+                MPI_Recv(&last_execute_recv, 1, ConvertType(getAbstractionDataType<R>()), rank_, rank_, MPI_COMM_WORLD, &mpi_status);
+                MPI_Recv(&y_recv, 1, MPI_INT, rank_, rank_+10, MPI_COMM_WORLD, &mpi_status);
+
+                // cout << rank_+1 << ": " << count_neighbors_recv << " " << last_execute_recv << " " << y_recv << endl;
+                for(int i = 0; i < count_neighbors_recv; i++)
+                    cs.memoria[y_recv + i].attribute.value += last_execute_recv;
             }
+
+            // Calculando o somatorio dos valores atributos resultante na particao do espaco celular
+            R acumulated_value_send = 0;
+            for(int i = 0; i < cs.height*cs.width; i++)
+                acumulated_value_send += cs.memoria[i].attribute.value;
+
+            // cout << "\t" << comm_rank << ": " << acumulated_value_send << endl;
+            MPI_Send(&acumulated_value_send, 1, ConvertType(getAbstractionDataType<R>()), MASTER, comm_rank, MPI_COMM_WORLD);
         }
     }
 };
